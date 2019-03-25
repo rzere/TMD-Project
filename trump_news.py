f9463504cbeed7aa99225fedfe2fb811159ec48d
Bharath <bharathsatheesh@gmail.com>
2019-03-25 17:16:11 -0400

    More raw data

--- trump_news.py
+++ trump_news.py
@@ -2,32 +2,14 @@ from newsapi import NewsApiClient
 import json
 
 # Init
-newsapi = NewsApiClient(api_key='1d28a210f7b24d87bd0c299d47b53553')
+nap = NewsApiClient(api_key='1d28a210f7b24d87bd0c299d47b53553')
 
-# /v2/top-headlines
-#top_headlines = newsapi.get_top_headlines(q='bitcoin',
-#                                           sources='bbc-news,the-verge',
-#                                           category='business',
-#                                           language='en',
-#                                           country='us')
-trump_articles = newsapi.get_everything(q='trump', from_param='2019-02-25', to='2019-03-25')
-# /v2/everything
-# all_articles = newsapi.get_everything(q='bitcoin',
-#                                       sources='bbc-news,the-verge',
-#                                       domains='bbc.co.uk,techcrunch.com',
-#                                       from_param='2017-12-01',
-#                                       to='2017-12-12',
-#                                       language='en',
-#                                       sort_by='relevancy',
-#                                       page=2)
-
-# /v2/sources
-#sources = newsapi.get_sources()
+# for index in range(1, 50):
+#     trump_articles = nap.get_everything(q='Trump',  from_param='2019-02-25', to='2019-03-25', page=index)
+#     with open('trump_news.json', 'a') as outfile:
+#         json.dump(trump_articles['articles'], outfile)
 
 
-with open('trump_news.json', 'w') as outfile:
-    json.dump(trump_articles, outfile)
-
-print(len(trump_articles))
-
-
+with open('trump_news.json') as json_file:
+    data = json.load(json_file)
+    print(data)
