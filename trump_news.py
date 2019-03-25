0496e9d9efb6b7c1c96771906d98268a9ad25027
Ruzgar Zere <rzere@rollins.edu>
2019-03-25 16:47:11 -0400

    save to json added to trump_news.py

--- trump_news.py
+++ trump_news.py
@@ -1,16 +1,16 @@
 from newsapi import NewsApiClient
+import json
 
 # Init
 newsapi = NewsApiClient(api_key='1d28a210f7b24d87bd0c299d47b53553')
 
 # /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
+#top_headlines = newsapi.get_top_headlines(q='bitcoin',
 #                                           sources='bbc-news,the-verge',
 #                                           category='business',
 #                                           language='en',
 #                                           country='us')
-trump_articles = newsapi.get_everything(q='trump', from_param='2019-02-25',
-                                        to='2019-03-25')
+trump_articles = newsapi.get_everything(q='trump', from_param='2019-02-25', to='2019-03-25')
 # /v2/everything
 # all_articles = newsapi.get_everything(q='bitcoin',
 #                                       sources='bbc-news,the-verge',
@@ -22,6 +22,12 @@ trump_articles = newsapi.get_everything(q='trump', from_param='2019-02-25',
 #                                       page=2)
 
 # /v2/sources
-sources = newsapi.get_sources()
+#sources = newsapi.get_sources()
 
-print(trump_articles)
+
+with open('trump_news.json', 'w') as outfile:
+    json.dump(trump_articles, outfile)
+
+print(len(trump_articles))
+
+
