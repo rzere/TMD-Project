3c9e54cade6ca479a204d7238f4dd477a6f463ec
Ruzgar Zere <rzere@rollins.edu>
2019-03-25 17:37:38 -0400

    added all sources (trying to iterate through sources and/or pages)

--- trump_news.py
+++ trump_news.py
@@ -5,12 +5,29 @@ import json
 newsapi = NewsApiClient(api_key='1d28a210f7b24d87bd0c299d47b53553')
 
 # /v2/top-headlines
-#top_headlines = newsapi.get_top_headlines(q='bitcoin',
+#top_headlines = newsapi.get_top_headlines(q='trump',
 #                                           sources='bbc-news,the-verge',
 #                                           category='business',
 #                                           language='en',
 #                                           country='us')
-trump_articles = newsapi.get_everything(q='trump', from_param='2019-02-25', to='2019-03-25')
+all_sources = ['the-new-york-times', 'breitbart-news', 'fox-news', 'the-american-conservative', 'daily-mail', 'the-huffington-post', 'the-washington-post', 'associated-press']
+
+
+# for i in range(len(all_sources)):
+#     articles = newsapi.get_everything(q='trump', sources=all_sources[i],
+#                                             from_param='2019-02-25', to='2019-03-25', language='en', page=index)
+#     with open('trump_news.json', 'w') as outfile:
+#         json.dump(articles, outfile)
+trump_articles = []
+for index in range(1, 99):
+    articles = newsapi.get_everything(q='Trump',  from_param='2019-02-25', to='2019-03-25', page=index)
+    trump_articles.append(articles)
+
+#trump_articles = newsapi.get_everything(q='Trump',  from_param='2019-02-25', to='2019-03-25', page=50)
+
+with open('trump_news.json', 'w') as outfile:
+    json.dump(trump_articles, outfile)
+
 # /v2/everything
 # all_articles = newsapi.get_everything(q='bitcoin',
 #                                       sources='bbc-news,the-verge',
@@ -25,9 +42,9 @@ trump_articles = newsapi.get_everything(q='trump', from_param='2019-02-25', to='
 #sources = newsapi.get_sources()
 
 
-with open('trump_news.json', 'w') as outfile:
-    json.dump(trump_articles, outfile)
-
-print(len(trump_articles))
+# with open('trump_news.json', 'w') as outfile:
+#     json.dump(all_articles, outfile)
+#
+# print(len(all_articles))
 
 
