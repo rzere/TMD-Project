53db052161ed3d8d02250bb8946ec7f3b765b4c6
Bharath <bharathsatheesh@gmail.com>
2019-03-28 16:10:42 -0400

    Finally recorded 800 data points

--- trump_news.py
+++ trump_news.py
@@ -2,6 +2,10 @@ from newsapi import NewsApiClient
 import pickle
 import pandas as pd
 import numpy as np
+import re
+import nltk
+from gensim.models import Word2Vec
+import itertools
 # Init
 newsapi = NewsApiClient(api_key='9f1e0b11257f4b14b0e18428c40a3e97')
 
@@ -34,4 +38,24 @@ except:
 # print(top_headlines)
 df = pd.DataFrame(final_list)
 df = df.replace(to_replace='None', value=np.nan).dropna()
+df.sort_values(by=['publishedAt'])
+final_list = list()
 
+# Cleaning the text
+raw_content = list(df['description'])
+for content in raw_content:
+    processed_article = content.lower()
+    processed_article = re.sub('[^a-zA-Z]', ' ', processed_article)
+    processed_article = re.sub(r'\s+', ' ', processed_article)
+    all_sentences = nltk.sent_tokenize(processed_article)
+    all_words = [nltk.word_tokenize(sent) for sent in all_sentences]
+    from nltk.corpus import stopwords
+    for i in range(len(all_words)):
+        all_words[i] = [w for w in all_words[i] if w not in stopwords.words('english')]
+    final_list.extend(all_words)
+
+# word_list = list(itertools.chain.from_iterable(final_list))
+word2vec = Word2Vec(final_list, min_count=1)
+vocabulary = word2vec.wv.vocab
+sim_words = word2vec.wv.most_similar('mueller')
+print(sim_words)
