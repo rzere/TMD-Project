dbf62efd54493a72cf8c917aa121c61dfc97a23c
Ruzgar Zere <rzere@rollins.edu>
2019-03-28 16:05:28 -0400

    tweets csv and json updated

--- trump_news.py
+++ trump_news.py
@@ -1,50 +1,36 @@
 from newsapi import NewsApiClient
-import json
-
+import pickle
+import pandas as pd
+import numpy as np
 # Init
-newsapi = NewsApiClient(api_key='1d28a210f7b24d87bd0c299d47b53553')
+newsapi = NewsApiClient(api_key='9f1e0b11257f4b14b0e18428c40a3e97')
 
 # /v2/top-headlines
-#top_headlines = newsapi.get_top_headlines(q='trump',
-#                                           sources='bbc-news,the-verge',
-#                                           category='business',
-#                                           language='en',
-#                                           country='us')
-all_sources = ['the-new-york-times', 'breitbart-news', 'fox-news', 'the-american-conservative', 'daily-mail', 'the-huffington-post', 'the-washington-post', 'associated-press']
-
-
-# for i in range(len(all_sources)):
-#     articles = newsapi.get_everything(q='trump', sources=all_sources[i],
-#                                             from_param='2019-02-25', to='2019-03-25', language='en', page=index)
-#     with open('trump_news.json', 'w') as outfile:
-#         json.dump(articles, outfile)
-trump_articles = []
-for index in range(1, 99):
-    articles = newsapi.get_everything(q='Trump',  from_param='2019-02-25', to='2019-03-25', page=index)
-    trump_articles.append(articles)
-
-#trump_articles = newsapi.get_everything(q='Trump',  from_param='2019-02-25', to='2019-03-25', page=50)
-
-with open('trump_news.json', 'w') as outfile:
-    json.dump(trump_articles, outfile)
+# for index in range(1, 11):
+#     print (index)
+#     top_headlines = newsapi.get_top_headlines(q='Trump', sources='google-news', page_size=100, page=index)
+#     print(top_headlines)
 
 # /v2/everything
-# all_articles = newsapi.get_everything(q='bitcoin',
-#                                       sources='bbc-news,the-verge',
-#                                       domains='bbc.co.uk,techcrunch.com',
-#                                       from_param='2017-12-01',
-#                                       to='2017-12-12',
-#                                       language='en',
-#                                       sort_by='relevancy',
-#                                       page=2)
+try:
+    with open('my_news_list.pkl', 'rb') as f:
+        final_list=pickle.load(f)
+except:
+    final_list = list()
+    for index in range(1, 11):
+        all_articles = newsapi.get_everything(q='Trump',
+                                              from_param='2019-02-29',
+                                              language='en',
+                                              sort_by='relevancy',
+                                              page_size=100,
+                                              page=index)
+        final_list.extend(list(all_articles['articles']))
+    with open('my_news_list.pkl', 'wb') as f:
+        pickle.dump(final_list, f)
 
 # /v2/sources
-#sources = newsapi.get_sources()
-
-
-# with open('trump_news.json', 'w') as outfile:
-#     json.dump(all_articles, outfile)
+# sources = newsapi.get_sources()
 #
-# print(len(all_articles))
-
-
+# print(top_headlines)
+df = pd.DataFrame(final_list)
+df = df.replace(to_replace='None', value=np.nan).dropna()
